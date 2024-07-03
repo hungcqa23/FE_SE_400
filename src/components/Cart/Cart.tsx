977244b35a09b1cc148302785e4fbca5bbdb2648
Hi <huydao1008@gmail.com>
2024-07-03 18:27:07 +0700

    add drawer

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,34 @@
-import { Link } from 'react-router-dom';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from 'src/components/ui/drawer';
+import { Button } from '../ui/button';
 
 export default function Cart() {
   return (
-    <Link to='/' className='flex flex-row items-center gap-3 border-2 ps-1 pe-4 rounded-full py-1'>
-      <img src='/cart.svg' alt='cart' className='w-[3rem] h-[3rem]' />
-      <span className='font-semibold'>Giỏ hàng (0)</span>
-    </Link>
+    <Drawer direction='right'>
+      <DrawerTrigger className='flex flex-row items-center gap-3 border-2 ps-1 pe-4 rounded-full py-1'>
+        <img src='/cart.svg' alt='cart' className='w-[3rem] h-[3rem]' />
+        <span className='font-semibold'>Giỏ hàng (0)</span>
+      </DrawerTrigger>
+      <DrawerContent className='h-screen top-0 right-0 left-auto mt-0 w-1/4 rounded-none'>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant='outline'>Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
