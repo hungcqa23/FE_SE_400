7f360d87eea217b4fe4756602350128d7d3a5e1a
Hi <huydao1008@gmail.com>
2024-07-06 02:43:29 +0700

    update zalopay feature

--- src/components/CartButton/CartButton.tsx
+++ src/components/CartButton/CartButton.tsx
@@ -2,7 +2,6 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
@@ -19,19 +18,19 @@ import { formatCurrency } from 'src/utils/formatCurrency';
 export default function CartButton() {
   const { data, error, isLoading } = useCartItems();
 
-  console.log(data);
-
-  const totalPrice = data?.reduce((acc, item) => acc + item.product.price, 0);
+  const totalPrice =
+    data == undefined ? 0 : data.reduce((acc, item) => acc + item.product.price, 0);
+  const totalQuantity = data == undefined ? 0 : data.length;
 
   return (
     <Drawer direction='right'>
       <DrawerTrigger className='flex flex-row items-center gap-3 border-2 ps-1 pe-4 rounded-full py-1'>
         <img src='/cart.svg' alt='cart' className='w-[3rem] h-[3rem]' />
-        <span className='font-semibold'>Giỏ hàng ({data?.length})</span>
+        <span className='font-semibold'>Giỏ hàng ({totalQuantity})</span>
       </DrawerTrigger>
       <DrawerContent className='h-screen top-0 right-0 left-auto mt-0 w-1/4 rounded-none'>
         <DrawerHeader className='flex flex-row justify-between items-center'>
-          <DrawerTitle>Giỏ hàng ({data?.length})</DrawerTitle>
+          <DrawerTitle>Giỏ hàng ({totalQuantity})</DrawerTitle>
           <DrawerClose>
             <svg
               width='25'
@@ -69,9 +68,9 @@ export default function CartButton() {
         </ScrollArea>
         <DrawerFooter>
           <div className='flex flex-row justify-between items-center'>
-            <span className='font-normal text-base text-[#1A1A1A]'>{data?.length} Sản phẩm</span>
+            <span className='font-normal text-base text-[#1A1A1A]'>{totalQuantity} Sản phẩm</span>
             <span className='font-semibold text-base text-[#1a1a1a]'>
-              {formatCurrency(totalPrice)}
+              {formatCurrency(totalPrice!)}
             </span>
           </div>
           <Link to='/checkout'>
--- src/components/CartDetail/CartDetail.tsx
+++ src/components/CartDetail/CartDetail.tsx
@@ -4,13 +4,23 @@ import CartDataTable from '../CartDataTable';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { Button } from '../ui/button';
 import { Link } from 'react-router-dom';
+import useCartItems from 'src/hooks/useCartItems';
+import { formatCurrency } from 'src/utils/formatCurrency';
 
 const CartDetail = () => {
+  const { data, error, isLoading } = useCartItems();
+
+  const totalPrice = data?.reduce((acc, item) => acc + item.product.price, 0);
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className='w-full flex flex-col items-center gap-8 mt-[6rem] mb-5'>
       <p className='text-3xl font-semibold text-[#1a1a1a]'>Giỏ hàng của tôi</p>
       <div className='flex gap-6 items-start'>
-        <CartDataTable />
+        <CartDataTable cartItems={data!} />
         <Card className='w-[480px]'>
           <CardHeader>
             <CardTitle className='text-xl'>Tổng giá trị giỏ hàng</CardTitle>
@@ -18,7 +28,7 @@ const CartDetail = () => {
           <CardContent className='flex flex-col gap-4'>
             <div className='flex justify-between'>
               <p className='text-sm text-muted-foreground'>Tổng tiền hàng: </p>
-              <p className='text-sm font-medium '>$26</p>
+              <p className='text-sm font-medium '>{formatCurrency(totalPrice!)}</p>
             </div>
             <div className='flex justify-between'>
               <p className='text-sm text-muted-foreground'>Phí ship: </p>
@@ -26,7 +36,7 @@ const CartDetail = () => {
             </div>
             <div className='flex justify-between'>
               <p className='text-base text-muted-foreground'>Tổng thanh toán: </p>
-              <p className='text-base font-medium '>$26</p>
+              <p className='text-base font-medium '>{formatCurrency(totalPrice!)}</p>
             </div>
           </CardContent>
           <CardFooter>
